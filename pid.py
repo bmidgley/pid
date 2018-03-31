d09b6a707a4ffc64f3ba425f7913296efa593268
Brad Midgley <bmidgley@gmail.com>
2018-03-31 12:32:57 -0600

    simplify

--- pid.py
+++ pid.py
@@ -31,29 +31,19 @@ RST = 24
 # 128x64 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-# Initialize library.
-disp.begin()
+# Create blank image for drawing.
 
-# Clear display.
+disp.begin()
 disp.clear()
 disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Load default font.
-font = ImageFont.load_default()
-#draw.text((x, top),    'Hello',  font=font, fill=255)
-#draw.text((x, top+20), 'World!', font=font, fill=255)
+# accel
 
 sensor = mpu6050(0x68)
 
