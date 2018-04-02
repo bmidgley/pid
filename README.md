7c24080abc63b7d7a14889af8d402cddd34983a8
Brad Midgley <bmidgley@gmail.com>
2018-04-02 15:47:29 -0600

    snake game with buzzer

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # pid pi (zero) game platform
 
-Requires an i2c ssd1306 display and a gy-521 accelerometer. 
+Requires an i2c ssd1306 display and a gy-521 accelerometer. Buzzer attaches to gnd and bcm23.
--- pid.py
+++ pid.py
@@ -10,8 +10,6 @@
 # sudo pip install RPi.GPIO Adafruit-SSD1306 mpu6050-raspberrypi
 # add to /etc/rc.local before exit: (python /home/pi/pid/pid.py) &
 
-import time
-
 import RPi.GPIO as GPIO
 from mpu6050 import mpu6050
 import Adafruit_GPIO.SPI as SPI
@@ -23,7 +21,9 @@ from PIL import ImageFont
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 GPIO.output(4, GPIO.HIGH)
+GPIO.output(23, GPIO.LOW)
 
 # Raspberry Pi pin configuration:
 RST = 24
@@ -41,26 +41,40 @@ width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
-draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 # accel
 
 sensor = mpu6050(0x68)
 
-x0 = 0
-y0 = 0
-x1 = 0
-y1 = 0
+x0 = 64
+y0 = 32
+
+snake = []
+
+length = 8
 
 while True:
+  draw.rectangle((0, 0, width - 1, height - 1), outline=255, fill=0)
+
   a = sensor.get_accel_data()
-  x1 = (x1 + a['y'])
-  y1 = (y1 - a['x'])
-  if (x1 == x1 % width and y1 == y1 % height):
-    draw.line((x0, y0, x1, y1), fill=255)
-  x1 = x0 = x1 % width
-  y1 = y0 = y1 % height
+  x1 = x0 + a['y']
+  y1 = y0 - a['x']
+  snake.append((x1, y1))
+
+  x0, y0 = snake[0]
+  for (x,y) in snake:
+    x = x % width
+    y = y % height
+    if (abs(x - x0) + abs(y - y0) < 20):
+      draw.line((x0, y0, x, y), fill=255)
+    x0 = x
+    y0 = y
 
   disp.image(image)
   disp.display()
+
+  if(len(snake) > length):
+    snake.pop(0)
+
+  GPIO.output(23, GPIO.HIGH if (x0 < 5 or y0 < 5) else GPIO.LOW)
 
